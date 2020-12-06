7e73f371248c5725cbe5fdae83b9b6880e047be7
Steph Janssen <steph.janssen@on2it.net>
2020-12-06 13:39:30 +0100

    Cleanup

--- 01B/main.go
+++ 01B/main.go
@@ -45,18 +45,3 @@ func readLines(path string) ([]string, error) {
 	}
 	return lines, scanner.Err()
 }
-
-// writeLines writes the lines to the given file.
-func writeLines(lines []string, path string) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	w := bufio.NewWriter(file)
-	for _, line := range lines {
-		fmt.Fprintln(w, line)
-	}
-	return w.Flush()
-}
